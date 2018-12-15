5434d5e39941423ee0e9c82a7f26ee5c8969868b
Maria Isabel Jiménez Velasco <i72jivem@uco.es>
2018-12-15 23:39:13 +0100

    Hechos main y fichero de los profesores permitidos

---
+++ CodigoPrograma/Explicación_Contenido_Fichero_Binario.md
@@ -0,0 +1,47 @@
+Los profesores que tienen acceso a la Base de Datos se encuentran enel fichero binario de profesores.
+
+Dichos profesores son:
+
+# Profesor 1:
+
+- *Dni:* 12345
+
+- *Contraseña:* pas1
+
+- *Rol:* 1
+
+
+# Profesor 2:
+
+ *Dni:* 23451
+
+ *Contraseña:* pas2
+
+ *Rol:* 2
+
+
+# Profesor 3:
+
+ *Dni:* 34512
+
+ *Contraseña:* pas3
+
+ *Rol:* 2
+
+# Profesor 4:
+
+ *Dni:* 45123
+
+ *Contraseña:* pas4
+
+ *Rol:* 1
+
+
+# Profesor 5:
+
+ *Dni:* 51234
+
+ *Contraseña:* pas5
+
+ *Rol:* 1
+
---
+++ CodigoPrograma/main.cc
@@ -0,0 +1,81 @@
+
+#include "Alumno.h"
+#include "Agenda.h"
+#include "Profesor.h"
+
+#include <string>
+#include <iostream>
+
+
+
+int main(){
+Profesor p;
+Agenda a;
+
+cout<<"Antes de iniciar sesion debe identificarse"<<endl;
+p.InsertarRol(0);
+p.Iniciar_Sesion();
+	
+    if(p.ObtenerRol()==1||p.ObtenerRol()==2){
+		
+		p.AsignarPuntero(&a);
+		p.CargarFicheroAlumnos();   
+	}
+   else{
+   	cout<<"Lo sentimos, no tiene permiso para acceder a la Base de Datos"<<endl;
+   	return -1;
+   }
+
+int eleccion;
+
+do{
+	cout<<"Que desea hacer"<<endl;
+
+	cout<<"1. Insertar Alumno "<<endl;
+	cout<<"2. Mostrar Alumno"<<endl;
+	cout<<"3. Modificar Alumno "<<endl;
+	cout<<"4. Borrar Alumno"<<endl;
+	cout<<"5. Guardar Base de datos"<<endl;
+	cout<<"6. Copia Seguridad "<<endl;
+	cout<<"7. Salir de la aplicación "<<endl;
+
+	cin>>eleccion;
+
+	switch(eleccion){
+		case 1:{
+				a.Insertar_Alumno();
+		}break;
+
+		case 2:{
+				a.Mostrar_Alumno();
+		}break;
+
+		case 3:{
+				a.Modificar_Alumno();
+		}break;
+
+		case 4:{
+				a.Borrar_Alumno();
+		}break;
+
+		case 5:{
+				p.GuardarFicheroAlumnos();
+		}break;
+
+		case 6:{
+				p.Copia_Seguridad();
+		}break;
+
+	}
+
+}while(eleccion<7);
+
+p.Salir_Aplicacion();
+				
+
+		
+
+
+
+
+}
